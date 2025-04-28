1968d740237bb7f6d52a3cd434bbd9ed9e9f8ebd
Davi Fernandes <davi.fer159@gmail.com>
2025-04-28 19:28:31 -0300

    feat: add login and register

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./style.module.css";
+
+export const Login = () => {
+  return(
+    <section className={styles.s_login}>
+      <h1>Login</h1>
+    </section>
+  )
+};
---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./style.module.css";
+
+export const Register = () => {
+  return (
+    <section className={styles.s_register}>
+      <h1>Register</h1>
+    </section>
+  );
+};
