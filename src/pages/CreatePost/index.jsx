a14e7ee2afa8f3b6ee89943e95e0c38811e677b6
Davi Fernandes <davi.fer159@gmail.com>
2025-04-29 18:33:00 -0300

    feat: create dashboard and post create

---
+++ src/pages/CreatePost/index.jsx
@@ -0,0 +1,8 @@
+import styles from "./style.module.css";
+
+export const CreatePost = () => {
+  return (
+    <section className={styles.s_createPost}>
+    </section>
+  );
+};
---
+++ src/pages/Dashboard/index.jsx
@@ -0,0 +1,8 @@
+import styles from "./style.module.css";
+
+export const Dashboard = () => {
+  return (
+    <section className={styles.s_dashboard}>
+    </section>
+  );
+};
